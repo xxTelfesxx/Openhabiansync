ec4333397e3db1de28b16fa80b13c357163e203f
xxTelfesxx <rene.denifl@gmail.com>
2018-06-21 14:01:03 +0200

    update fabtest

--- Farbentest.py
+++ Farbentest.py
@@ -9,28 +9,26 @@ GPIO.setmode(GPIO.BCM)
 comand_line_arg = sys.argv
 
 wartezeit = float(comand_line_arg[1])/1000
+green = float(comand_line_arg[2])
+red = float(comand_line_arg[3])
+blue = float(comand_line_arg[4])
+
+GPIO.setup(14, GPIO.OUT) #grün
+GPIO.setup(15, GPIO.OUT) #rot
+GPIO.setup(18, GPIO.OUT) #blau
 
 
-GPIO.setup(14, GPIO.OUT)
-GPIO.setup(15, GPIO.OUT)
-GPIO.setup(18, GPIO.OUT)
+p_green = GPIO.PWM(14, 100) #grün
+p_red = GPIO.PWM(15, 100) #rot
+p_blue = GPIO.PWM(18, 100) #blau
 
 
-
-GPIO.output(18, GPIO.HIGH)
+p_green.start(green)
+p_red.start(red)
+p_blue.start(blue)
 
 time.sleep(wartezeit)
 
-GPIO.output(14, GPIO.HIGH)
-
-time.sleep(wartezeit)
-
-GPIO.output(15, GPIO.HIGH)
-
-time.sleep(wartezeit)
-
-GPIO.output(18, GPIO.LOW)
-GPIO.output(14, GPIO.LOW)
-GPIO.output(15, GPIO.LOW)
-
-print("gloffen", wartezeit)
+p_green.stop()
+p_red.stop()
+p_blue.stop()

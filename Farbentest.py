0d59406c06f69a9e74e6d487f83633ee17fbdf3f
xxTelfesxx <rene.denifl@gmail.com>
2018-06-21 18:28:31 +0200

    fullpath

--- Farbentest.py
+++ Farbentest.py
@@ -4,7 +4,7 @@ import time
 import numpy
 import os, signal
 
-data = numpy.genfromtxt("colorstate.txt")
+data = numpy.genfromtxt("/home/openhabian/gitsync/Openhabiansynccolorstate.txt")
 ein_aus = int(data[0])
 status_red = int(data[1])
 status_green = int(data[2])
@@ -12,7 +12,7 @@ status_blue = int(data[3])
 status_brightness = int(data[4])
 pidvorgaenger = int(data[5])
 
-outputfile = open("colorstate.txt", "w")
+outputfile = open("/home/openhabian/gitsync/Openhabiansynccolorstate.txt", "w")
 
 while True:
 	try:

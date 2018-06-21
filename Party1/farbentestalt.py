8c004a44403380d5d6cf9ee67f936381c3d9935d
xxTelfesxx <rene.denifl@gmail.com>
2018-06-22 00:44:51 +0200

    asd

--- Party1/farbentestalt.py
+++ Party1/farbentestalt.py
@@ -1,4 +1,3 @@
-import sys
 import RPi.GPIO as GPIO
 import time
 
@@ -16,18 +15,10 @@ def blink(zeit):
 
 GPIO.setmode(GPIO.BCM)
 
-comand_line_arg = sys.argv
-
-green = float(comand_line_arg[1])
-red = float(comand_line_arg[2])
-blue = float(comand_line_arg[3])
-
-
 
 GPIO.setup(18, GPIO.OUT) #gruen
 GPIO.setup(14, GPIO.OUT) #rot
 GPIO.setup(15, GPIO.OUT) #blau
-
 
 
 p_red = GPIO.PWM(14, 100)

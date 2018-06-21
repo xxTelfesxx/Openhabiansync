7c065ffd9ef0806c5a0a5e3e71f655a5dce3d174
xxTelfesxx <rene.denifl@gmail.com>
2018-06-21 14:02:19 +0200

    non asci bug

--- Farbentest.py
+++ Farbentest.py
@@ -13,12 +13,12 @@ green = float(comand_line_arg[2])
 red = float(comand_line_arg[3])
 blue = float(comand_line_arg[4])
 
-GPIO.setup(14, GPIO.OUT) #grün
+GPIO.setup(14, GPIO.OUT) #gruen
 GPIO.setup(15, GPIO.OUT) #rot
 GPIO.setup(18, GPIO.OUT) #blau
 
 
-p_green = GPIO.PWM(14, 100) #grün
+p_green = GPIO.PWM(14, 100) #gruen
 p_red = GPIO.PWM(15, 100) #rot
 p_blue = GPIO.PWM(18, 100) #blau
 

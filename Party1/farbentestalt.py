51da86355d4cea2ed11b6e2cd6991816e2a5a030
xxTelfesxx <rene.denifl@gmail.com>
2018-06-22 08:31:35 +0200

    party update auf zeitspünge messen

--- Party1/farbentestalt.py
+++ Party1/farbentestalt.py
@@ -1,5 +1,6 @@
-import RPi.GPIO as GPIO
+#import RPi.GPIO as GPIO
 import time
+import os
 
 def blink():
 	p_green.start(100)
@@ -12,22 +13,38 @@ def blink():
 	p_red.stop()
 	p_blue.stop()
 
-
-GPIO.setmode(GPIO.BCM)
-
-
-GPIO.setup(18, GPIO.OUT) #gruen
-GPIO.setup(14, GPIO.OUT) #rot
-GPIO.setup(15, GPIO.OUT) #blau
+def gpio_setup():
+	GPIO.setmode(GPIO.BCM)
 
 
-p_red = GPIO.PWM(14, 100)
-p_blue = GPIO.PWM(15, 100)
-p_green = GPIO.PWM(18, 100)
+	GPIO.setup(18, GPIO.OUT) #gruen
+	GPIO.setup(14, GPIO.OUT) #rot
+	GPIO.setup(15, GPIO.OUT) #blau
+
+
+	p_red = GPIO.PWM(14, 100)
+	p_blue = GPIO.PWM(15, 100)
+	p_green = GPIO.PWM(18, 100)
+
+
+#gpio_setup()
 
 zeitmessung = 0
 
 inputfile = open("Textspur.txt")
+
+zeitstempel = []
+wartezeit = []
+
+for i in range(73):
+	info = inputfile.readline()
+	zeitstempel.append((float(info[0:7])))
+
+	
+for ii in range(72):
+	wartezeit.append(zeitstempel[ii+1]-zeitstempel[ii])
+	
+	
 
 print("3")
 time.sleep(1)
@@ -37,15 +54,17 @@ print("1")
 time.sleep(1)
 print("GOOOOOOOOOOOO")
 
-for i in range(77):
-	inf = inputfile.readline()
-	inf = float(inf[0:7])
-	
-	while zeitmessung < inf:
-		zeitmessung = zeitmessung +0.001
-		time.sleep(0.001)
-	
-	blink()
-	zeitmessung = zeitmessung +0.01
+os.system("start does_your_mother_knowcut.mp3")
+
+time.sleep(0.01)
+
+print(wartezeit)
+
+for iwartezeit in wartezeit:
+	time.sleep(iwartezeit-0.02)
+		
+	#blink()
+	print("OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO")
+	print(" ")
 	
 

44854ea6816ba7cac258d721d92fe7c9a35911c8
xxTelfesxx <rene.denifl@gmail.com>
2018-09-10 11:08:54 +0200

    cron "" vergessen

--- alarm-rule-generator.py
+++ alarm-rule-generator.py
@@ -14,7 +14,7 @@ outputfile = open("/etc/openhab2/rules/" + nametxt + ".rules", "w")
 		
 
 outputfile.write('Rule "Abendallarm"' + "\n")
-outputfile.write('when Time cron 0 ' + minuten +" " + stunden + "* * ? *" + "\n")
+outputfile.write('when Time cron "0 ' + minuten +" " + stunden + ' 1/1 * ? *"' + "\n")
 outputfile.write("then Blinds_controll=2" + "\n")
 outputfile.write("end")
 outputfile.close()

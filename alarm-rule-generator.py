5e6d6dff3a92fcc49cc22eb1f5e5a043012f00b1
xxTelfesxx <rene.denifl@gmail.com>
2018-09-10 11:03:25 +0200

    int str fehler

--- alarm-rule-generator.py
+++ alarm-rule-generator.py
@@ -6,15 +6,15 @@ import sys
 comand_line_arg = sys.argv
 
 nametxt = str(comand_line_arg[1])
-stunden = int(comand_line_arg[2])
-minuten = int(comand_line_arg[3])
+stunden = str(comand_line_arg[2])
+minuten = str(comand_line_arg[3])
 
 
 outputfile = open("/etc/openhab2/rules/" + nametxt + ".rules", "w")
 		
 
 outputfile.write('Rule "Abendallarm"' + "\n")
-outputfile.write('when Time cron 0 ' +minuten +" " + stunden + "* * ? *" + "\n")
+outputfile.write('when Time cron 0 ' + minuten +" " + stunden + "* * ? *" + "\n")
 outputfile.write("then Blinds_controll=2" + "\n")
 outputfile.write("end")
 outputfile.close()

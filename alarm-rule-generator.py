3411d0d39aaad23fab1290f5492d72af54f53be3
xxTelfesxx <rene.denifl@gmail.com>
2018-09-10 11:10:44 +0200

    syntax fehler

--- alarm-rule-generator.py
+++ alarm-rule-generator.py
@@ -13,7 +13,7 @@ minuten = str(comand_line_arg[3])
 outputfile = open("/etc/openhab2/rules/" + nametxt + ".rules", "w")
 		
 
-outputfile.write('Rule "Abendallarm"' + "\n")
+outputfile.write('rule "Abendallarm"' + "\n")
 outputfile.write('when Time cron "0 ' + minuten +" " + stunden + ' 1/1 * ? *"' + "\n")
 outputfile.write("then Blinds_controll=2" + "\n")
 outputfile.write("end")

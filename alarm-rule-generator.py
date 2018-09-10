ccc0151d49e265ef1fffa23c714a780947123f1b
xxTelfesxx <rene.denifl@gmail.com>
2018-09-10 11:18:10 +0200

    auf ab in einem script

--- alarm-rule-generator.py
+++ alarm-rule-generator.py
@@ -1,21 +1,21 @@
 import numpy
 import sys
 			
-# eingabe: [Name txt Datei] [Stunden] [Minuten]
+# eingabe: [Name txt Datei] [Stunden] [Minuten] [auf ab? 0= auf, 2 = ab]
 
 comand_line_arg = sys.argv
 
 nametxt = str(comand_line_arg[1])
 stunden = str(comand_line_arg[2])
 minuten = str(comand_line_arg[3])
-
+mode = str(comand_line_arg[4])
 
 outputfile = open("/etc/openhab2/rules/" + nametxt + ".rules", "w")
 		
 
 outputfile.write('rule "Abendallarm"' + "\n")
 outputfile.write('when Time cron "0 ' + minuten +" " + stunden + ' 1/1 * ? *"' + "\n")
-outputfile.write("then sendCommand(Blinds_controll, 2)" + "\n")
+outputfile.write("then sendCommand(Blinds_controll, " + mode + ")" + "\n")
 outputfile.write("end")
 outputfile.close()
 
